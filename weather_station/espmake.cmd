2f3065c6f898f10c22f756588f0ad0214ad31c3c
liviuflore <liviuflore@gmail.com>
2015-03-31 18:16:33 +0300

    small changes

--- weather_station/espmake.cmd
+++ weather_station/espmake.cmd
@@ -1,11 +1,13 @@
 @echo off
+
 set TOOLKIT_DIR=D:\_code\esp\toolkit
+PATH=%PATH%;%TOOLKIT_DIR%\xtensa-lx106-elf\bin;C:\MinGW\bin;C:\MinGW\msys\1.0\bin
+
+
 SET PROJECT_NAME=%1
 SET PROJECT_DIR=%~dp0
 
-PATH=%PATH%;%TOOLKIT_DIR%\xtensa-lx106-elf\bin;C:\MinGW\bin;C:\MinGW\msys\1.0\bin
-
 rem generate webpages
-%PROJECT_DIR%\src\www\htmltoc.exe .\src\www\html\ .\src\www\
+%PROJECT_DIR%\src\www\htmltoc.exe %PROJECT_DIR%\src\www\html\ %PROJECT_DIR%\src\www\
 
 make %2 %3 %4 %5
