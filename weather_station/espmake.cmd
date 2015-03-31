6630a9dd60ec357616f0dd046ea6b82baf61a51d
liviuflore <liviuflore@gmail.com>
2015-03-31 19:13:12 +0300

    relative path for toolkit dir

--- weather_station/espmake.cmd
+++ weather_station/espmake.cmd
@@ -1,13 +1,16 @@
 @echo off
 
-set TOOLKIT_DIR=D:\_code\esp\toolkit
-PATH=%PATH%;%TOOLKIT_DIR%\xtensa-lx106-elf\bin;C:\MinGW\bin;C:\MinGW\msys\1.0\bin
-
+PATH=%PATH%;C:\MinGW\bin;C:\MinGW\msys\1.0\bin
 
 SET PROJECT_NAME=%1
 SET PROJECT_DIR=%~dp0
 
-rem generate webpages
-%PROJECT_DIR%\src\www\htmltoc.exe %PROJECT_DIR%\src\www\html\ %PROJECT_DIR%\src\www\
+set TOOLKIT_DIR=%PROJECT_DIR%\..\..\esp_toolkit\
+PATH=%PATH%;%TOOLKIT_DIR%\xtensa-lx106-elf\bin;
+
+rem generate webpages if building all
+IF [%2]==[] (
+    %PROJECT_DIR%\src\www\htmltoc.exe %PROJECT_DIR%\src\www\html\ %PROJECT_DIR%\src\www\
+)
 
 make %2 %3 %4 %5
