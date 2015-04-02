9c097dde551eb90dc50efb3e6be782fb9cb27e36
liviuflore <liviuflore@gmail.com>
2015-04-02 19:51:55 +0300

    add user_config skeleton

---
+++ weather_station/src/user_config.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+* Copyright 2015 liv
+*
+* FileName: user_config.c
+*
+* Description: stores, loads configuration from flash
+*
+*******************************************************************************/
+#ifdef WIN32
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#else
+#include "esp_common.h"
+#endif
+#include "debug.h"
+
+struct u_config esp_config = { 0 };
+
+
+void user_config_load (void)
+{
+
+}
+
+void user_config_save (void)
+{
+
+}
--- weather_station/src/user_config.h
+++ weather_station/src/user_config.h
@@ -4,5 +4,25 @@
 #define MYSSID        "myssid"
 #define MYPASSPHRASE  "password"
 
+#define U_CONFIG_SSID_MAX           32
+#define U_CONFIG_WIFIPASS_MAX       64
+
+/* Configuration parameters header structure */
+struct u_config_header {
+    unsigned char version;
+    unsigned char length;
+};
+
+/* Configuration parameters main structure */
+struct u_config {
+    unsigned char version;
+    unsigned char length;
+
+    char ssid[U_CONFIG_SSID_MAX];
+    char wifi_pass[U_CONFIG_WIFIPASS_MAX];
+};
+
+
+
 #endif
 
